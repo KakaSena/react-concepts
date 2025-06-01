1f087d3be36069f6b3b61cbd3c8435c233ca2e5b
Kaká Xavier Sena <kakaxsena@gmail.com>
2025-06-01 00:26:11 +0000

    css styling & dynamic styling

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,7 @@ function App() {
   const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {
-    // selectedButton => 'components', 'jsx', 'props', 'state'
     setSelectedTopic(selectedButton);
-    // console.log(selectedTopic);
   }
 
   console.log("APP COMPONENT EXECUTING");
