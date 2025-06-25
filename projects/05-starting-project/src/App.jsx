05bf4c3eb3e689154ee109fb3eb132df404a3910
Kaká Xavier Sena <kakaxsena@gmail.com>
2025-06-25 17:08:42 -0300

    new project component

--- projects/05-starting-project/src/App.jsx
+++ projects/05-starting-project/src/App.jsx
@@ -1,8 +1,10 @@
+import NewProject from "./components/NewProject";
 import ProjectSidebar from "./components/ProjectsSidebar";
 function App() {
   return (
-    <main className="h-screen my-8">
-      <ProjectSidebar></ProjectSidebar>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectSidebar />
+      <NewProject />
     </main>
   );
 }
