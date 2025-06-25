fdd86d10293aa20a1a474101ac72e0d3b66afd33
Kaká Xavier Sena <kakaxsena@gmail.com>
2025-06-25 16:42:31 -0300

    sidebar and new project component

---
+++ projects/05-starting-project/src/components/NewProject.jsx
@@ -0,0 +1,28 @@
+export default function NewProject() {
+  return (
+    <div>
+      <menu>
+        <li>
+          <button>Cancel</button>
+          <button>Save</button>
+        </li>
+      </menu>
+      <div>
+        <p>
+          <label>Title</label>
+          <input type="text" name="" id="" />
+        </p>
+
+        <p>
+          <label>Description</label>
+          <textarea name="" id=""></textarea>
+        </p>
+
+        <p>
+          <label>Due Date</label>
+          <input type="text" name="" id="" />
+        </p>
+      </div>
+    </div>
+  );
+}
