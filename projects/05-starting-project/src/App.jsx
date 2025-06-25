7a23e69eb49e0a7be807b240c77bb027672a491f
Kaká Xavier Sena <kakaxsena@gmail.com>
2025-06-25 16:41:39 -0300

    initial setupt

---
+++ projects/05-starting-project/src/App.jsx
@@ -0,0 +1,10 @@
+import ProjectSidebar from "./components/ProjectsSidebar";
+function App() {
+  return (
+    <main className="h-screen my-8">
+      <ProjectSidebar></ProjectSidebar>
+    </main>
+  );
+}
+
+export default App;
